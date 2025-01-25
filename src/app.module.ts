5e79cad1e403eff4c60f5d259e462cc5ff33a0cf
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-01-25 15:38:27 +0800

    feat: connect to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      entities: [],
+      username: 'postgres',
+      password: 'Blackkuro01#',
+      database: 'Nur',
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
